1abea4024b2b186827731c2ca1fdaa6bb4066f18
Mark Eschbach <meschbach@gmail.com>
2015-11-19 22:26:23 -0800

    Add command line flags for checking and expiry notifications

--- check-cert.go
+++ check-cert.go
@@ -2,30 +2,39 @@ package main
 
 import (
 	"crypto/tls"
+	"flag"
 	"fmt"
 	"log"
 	"time"
 )
 
-const notify int64 = 220
+type SiteCheck struct {
+	notify int64
+	sites []string
+}
 
 func main() {
-	sites := []string{
-		"okta.okta.com:443",
-		"authbank.com:443",
-		"uber.com:443",
-	}
+	check := new(SiteCheck)
+	parseArguments( check )
 
 	config := tls.Config{InsecureSkipVerify: false}
-	for _, site := range sites {
-		dosite(site, config)
+	for _, site := range check.sites {
+		dosite(site, check.notify, config)
 	}
 }
-func dosite(site string, config tls.Config) {
+
+func parseArguments( check *SiteCheck ) {
+	flag.Int64Var( &check.notify, "notify", 220, "Notify if fewer than this number of days are presented" )
+	flag.Parse()
+	check.sites = flag.Args()
+}
+
+func dosite(site string, notify int64,  config tls.Config) {
 	conn, err := tls.Dial("tcp", site, &config)
 	if err != nil {
-		log.Fatalf("client: %s", err)
+		log.Fatalf("client(%s): %s", site, err)
 	}
+
 	defer conn.Close()
 	state := conn.ConnectionState()
 	for pos, cert := range state.PeerCertificates {
@@ -33,12 +42,15 @@ func dosite(site string, config tls.Config) {
 			diff := cert.NotAfter.Unix() - time.Now().Unix()
 			diff /= 86400
 			certkind := "Certificate"
+
 			if pos > 0 {
 				certkind = "Certificate authority"
 			}
+
 			if diff < notify {
 				fmt.Printf("%s %s expires in %d days at %s (warning at %d days)\n", site, certkind, diff, cert.NotAfter, notify)
 			}
+
 		}
 	}
 }
